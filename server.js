22dc38fb4681e115682d4760cc8c7a5ac1dcdc64
RayCurse <38709018+RayCurse@users.noreply.github.com>
2023-04-27 12:29:09 -0400

    Add basic endpoints

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ import minimist from 'minimist'
 import express from 'express'
 
 const port = minimist(process.argv.slice(2)).port || 5000;
-console.log(port)
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+})
 
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
